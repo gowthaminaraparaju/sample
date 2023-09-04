8e297b67956b4637152688b5a2acfc886378c3fb
gowthami <gowthami@LAPTOP-54CGIQF6>
2023-09-03 22:15:48 -0700

    First  commit

---
+++ src/test/java/tests/FirstTest.java
@@ -0,0 +1,13 @@
+package tests;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class FirstTest {
+	@Test
+	public void test1()
+	{
+		Reporter.log("first test-1");
+	}
+
+}
